79053c1096a2b22b3a4dfd7ce2b2f7092789b398
いとうけいた <keita.itou2@grp.tmnf.jp>
2022-03-01 16:41:52 +0900

    [FIZZBUZZ]

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -17,40 +17,39 @@ hello = 3 , world = 4
  *  lines: Array<string> 入力された行(末尾は必ず改行)分の配列
  */
 const main = () => {
-    const N = lines[0]
-    for(let i = 1; i <= N; i++) {
-        if(i % 3 == 0){
-            if(i % 5 == 0){
-                console.log("Fizz Buzz")
-            }
-            else{
-                console.log("Fizz")
-                } 
-            }else if(i % 5 == 0){
-                console.log("Buzz")
-            }else{
-                console.log(i)
-            }
-        }
-    // 空行
-    console.log('')
-}
+  const N = lines[0];
+  for (let i = 1; i <= N; i++) {
+    if (i % 3 == 0) {
+      if (i % 5 == 0) {
+        console.log("Fizz Buzz");
+      } else {
+        console.log("Fizz");
+      }
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  // 空行
+  console.log("");
+};
 
 /**
  * 標準入出力のための処理
  */
-const reader = require('readline')
+const reader = require("readline");
 
-process.stdin.resume()　//入力待ち状態にする
-process.stdin.setEncoding('utf8')
+process.stdin.resume(); //入力待ち状態にする
+process.stdin.setEncoding("utf8");
 const rl = reader.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-let lines = []
-rl.on('line', (line) => {
-    lines.push(line)
-})
+let lines = [];
+rl.on("line", (line) => {
+  lines.push(line);
+});
 
-rl.on('close', main)
+rl.on("close", main);
