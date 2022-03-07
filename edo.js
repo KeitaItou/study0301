6026fa49e8cda18051b6c2926c9181d75e98d7e2
いとうけいた <keita.itou2@grp.tmnf.jp>
2022-03-07 19:48:06 +0900

    ウナギ屋修正（正解かどうか未確認）

--- edo.js
+++ edo.js
@@ -18,26 +18,31 @@ hello = 3 , world = 4
  */
 const main = () => {
   const N = lines[0].split(" ");
+  //席数の取得
   const seat = parseInt(N[0]);
+  //グループ数の取得
   const NumberOfGroups = parseInt(N[1]);
   let result = [];
+  //すでに人が座っている席かどうか判断するための配列を定義
+  //初期値は0で、座ると1になる
   for (let a = 0; a < seat; a++) {
     result[a] = 0;
   }
   for (let i = 1; i <= NumberOfGroups; i++) {
     let count = 0;
+    //グループの人数と着席開始番号を取得する配列を定義
     const line = lines[i].split(" ");
     const NumberOfMembers = parseInt(line[0]);
     const StartSeat = parseInt(line[1]) - 1;
     if (NumberOfMembers + StartSeat <= seat) {
       for (let j = StartSeat; j < NumberOfMembers + StartSeat; j++) {
-        if (result[j] == 0) {
+        if (result[j] === 0) {
           count += 1;
         }
-        if (count == NumberOfMembers) {
-          for (let k = StartSeat; k < NumberOfMembers + StartSeat; k++) {
-            result.splice(k, 1, 1);
-          }
+      }
+      if (count === NumberOfMembers) {
+        for (let k = StartSeat; k < NumberOfMembers + StartSeat; k++) {
+          result.splice(k, 1, 1);
         }
       }
     } else {
@@ -45,24 +50,24 @@ const main = () => {
         if (result[l] == 0) {
           count += 1;
         }
-        for (let m = 0; m < NumberOfMembers + StartSeat - seat; m++) {
-            if (result[m] == 0) {
-                count += 1;
-            }
-        if (count == NumberOfMembers) {
-          for (let n = StartSeat; n < seat; n++) {
-            result.splice(n, 1, 1);
-          }
-          for (let o = 0; o < NumberOfMembers + StartSeat - seat; o++) {
-            result.splice(o, 1, 1);
-          }
+      }
+      for (let m = 0; m < NumberOfMembers + StartSeat - seat; m++) {
+        if (result[m] === 0) {
+          count += 1;
+        }
+      }
+      if (count === NumberOfMembers) {
+        for (let n = StartSeat; n < seat; n++) {
+          result.splice(n, 1, 1);
+        }
+        for (let o = 0; o < NumberOfMembers + StartSeat - seat; o++) {
+          result.splice(o, 1, 1);
         }
       }
     }
   }
-}
 
-const init = 0;
+  const init = 0;
   const customer = result.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     init
@@ -89,4 +94,4 @@ rl.on("line", (line) => {
   lines.push(line);
 });
 
-rl.on("close", main)
+rl.on("close", main);
